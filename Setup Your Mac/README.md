a02499d938f5f9b63188b23f1c440a1ab883d11d
Dan K. Snelson <dan@snelson.us>
2022-05-20 05:41:28 -0600

    v1.1.0

--- Setup Your Mac/README.md
+++ Setup Your Mac/README.md
@@ -1,6 +1,6 @@
 # Setup Your Mac via swiftDialog
 
-Leverages [swiftDialog](https://github.com/bartreardon/swiftDialog/releases) v1.10.1 (or later) and Jamf Pro Policy [Custom Events](https://docs.jamf.com/10.36.0/jamf-pro/documentation/Policy_Management.html?hl=custom%2Cevent#ID-0001f43f) to allow end-users to self-complete Mac setup post-enrollment via Jamf Pro's Self Service. (See Jamf Pro Known Issues PI100009 - PI-004775.)
+> Leverages [swiftDialog](https://github.com/bartreardon/swiftDialog/releases) v1.10.1 (or later) and Jamf Pro Policy [Custom Events](https://docs.jamf.com/10.36.0/jamf-pro/documentation/Policy_Management.html?hl=custom%2Cevent#ID-0001f43f) to allow end-users to self-complete Mac setup **post-enrollment** via Jamf Pro's Self Service. (See Jamf Pro Known Issues `PI100009 - PI-004775`.)
 
 Inspired by:
 - Rich Trouton ([@rtrouton](https://github.com/rtrouton))
@@ -21,3 +21,71 @@ Based on:
 ### Harvesting Self Service Icons
 
 [<img alt="Setup Your Mac via swiftDialog" width="650px" src="images/Harvesting_Self_Service_Icons.png" />](https://rumble.com/v119x6y-harvesting-self-service-icons.html)
+
+---
+
+## Features
+
+### Asset Tag Capture
+
+Version `1.1.0` prompts the user to provide an Asset Tag, which is captured and reported in the **Setting up your Mac** screen and submitted near the end of the scipt when inventory is updated via `jamf recon -assetTag ${assetTag}`.
+
+![Asset Tag Capture](images/Asset_Tag_Capture.png "Asset Tag Capture")
+ ### Debug Mode
+
+Version `1.1.0` introduces **Debug Mode**, which may prove helpful during initial deployment or for ongoing troubleshooting.
+
+When Debug Mode is set to `true` — and only `true` — you will observe the following behavior changes:
+- The `--title` in the **Setting up your Mac** screen will be preceeded by **DEBUG MODE |**
+- Jamf Pro policies will **not** execute, but will output to the Jamf Pro Policy Log what _would_ have executed had Debug Mode not been set to `true`
+- Computer inventory will **not** be updated, but will output to the Jamf Pro Policy Log what _would_ have been submitted (i.e., user-entered Asset Tag)
+
+**Note:** When Debug Mode is set to `true`, you should expect to observe multiple failures in the **Setting up your Mac** screen as the policies which install the so-called `filepath for validation` don't actually execute.
+
+![Debug Mode](images/Debug_Mode.png "Debug Mode")
+
+---
+
+## Configuration
+
+You should be comfortable _modifying_ scripts before using this approach (i.e., customizing existing settings to your needs).
+
+If the core functionality of this script meets your needs, **writing** code should not be required.
+
+### Script Modifications
+
+#### `apps` Variable
+
+For each configuration step, enter a pipe-separated list of:
+- Display Name
+- Filepath for validation
+- Jamf Pro Policy Custom Event Name
+- [Icon hash](https://rumble.com/v119x6y-harvesting-self-service-icons.html)
+
+##### Example
+
+```
+apps=(
+    "FileVault Disk Encryption|/Library/Preferences/com.apple.fdesetup.plist|filevault|f9ba35bd55488783456d64ec73372f029560531ca10dfa0e8154a46d7732b913"
+    "Sophos Endpoint|/Applications/Sophos/Sophos Endpoint.app|sophosEndpoint|c70f1acf8c96b99568fec83e165d2a534d111b0510fb561a283d32aa5b01c60c"
+    "Palo Alto GlobalProtect|/Applications/GlobalProtect.app|globalProtect|fcccf5d72ad9a4f6d3a4d780dcd8385378a0a8fd18e8c33ad32326f5bd53cca0"
+    "Google Chrome|/Applications/Google Chrome.app|googleChrome|12d3d198f40ab2ac237cff3b5cb05b09f7f26966d6dffba780e4d4e5325cc701"
+    "Microsoft Teams|/Applications/Microsoft Teams.app|microsoftTeams|dcb65709dba6cffa90a5eeaa54cb548d5ecc3b051f39feadd39e02744f37c19e"
+    "Zoom|/Applications/zoom.us.app|zoom|92b8d3c448e7d773457532f0478a428a0662f694fbbfc6cb69e1fab5ff106d97"
+)
+```
+
+### Jamf Pro Script Options
+
+![Jamf Pro Script Options](images/Parameter_Labels.png "Jamf Pro Script Options")
+
+> Labels to use for script parameters. Parameters 1 through 3 are predefined as mount point, computer name, and username
+
+- **Parameter 4:** `Debug Mode ( true | false, blank)`
+
+### Jamf Pro Policy Script Payload
+
+![Jamf Pro Policy Script Payload](images/Policy_Script_Payload.png "Jamf Pro Policy Script Payload")
+
+- **Debug Mode:** `true` to enable Debug Mode; _anything else_ to disable
+
--- Setup Your Mac/Setup-Your-Mac-via-Dialog.bash
+++ Setup Your Mac/Setup-Your-Mac-via-Dialog.bash
@@ -16,28 +16,11 @@
 #
 # HISTORY
 #
-# Version 0.0.1, 19-Mar-2022, Dan K. Snelson (@dan-snelson)
-#   Original version
+# Version 1.0.0, 30-Apr-2022, Dan K. Snelson (@dan-snelson)
+#   First "official" release
 #
-# Version 0.0.2, 20-Mar-2022, Dan K. Snelson (@dan-snelson)
-#   Corrected initial indeterminate progress bar. (Thanks, @bartreardon!)
-#
-# Version 0.0.3, 21-Mar-2022, Dan K. Snelson (@dan-snelson)
-#   Re-corrected initial indeterminate progress bar.
-#
-# Version 0.0.4, 16-Apr-2022, Dan K. Snelson (@dan-snelson)
-#   Updated for Listview processing https://github.com/bartreardon/swiftDialog/pull/103
-#   Added dynamic, policy-based icons
-#
-# Version 0.0.5, 21-Apr-2022, Dan K. Snelson (@dan-snelson)
-#   Standardized references to listitem code to more easily leverage statustext
-#   Simplified "jamf policy -event" code
-#
-# Version 0.0.6, 22-Apr-2022, Dan K. Snelson (@dan-snelson)
-#   Added error-checking to appCheck (thanks for the idea, @@adamcodega!)
-#
-# Version 0.0.7, 29-Apr-2022, Dan K. Snelson (@dan-snelson)
-#   Mapped v1.10.2's new "--quitkey" to command+b (because "b" is for Bart!)
+# Version 1.1.0, 19-May-2022, Dan K. Snelson (@dan-snelson)
+#   Added initial Splash screen with Asset Tag Capture and Debug Mode
 #
 ####################################################################################################
 
@@ -50,17 +33,32 @@
 ####################################################################################################
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-# Dialog Title and Message
+# Script Version & Debug Mode
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
-title="Setting up your Mac"
-message="Please wait while the following apps are downloaded and installed:"
+scriptVersion="1.1.0"
+debugMode="${4}"        # ( true | false, blank )
 
 
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-# For each configuration step (i.e., app to be installed), enter a pipe-separated list of:
+# Set Dialog path, Command Files and currently logged-in user
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+dialogApp="/usr/local/bin/dialog"
+dialog_command_file="/var/tmp/dialog.log"
+welcome_screen_command_file="/var/tmp/dialog_welcome_screen.log"
+loggedInUser=$( /bin/echo "show State:/Users/ConsoleUser" | /usr/sbin/scutil | /usr/bin/awk '/Name :/ { print $3 }' )
+
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# APPS TO BE INSTALLED
+#
+# For each configuration step, enter a pipe-separated list of:
 # Display Name | Filepath for validation | Jamf Pro Policy Custom Event Name | Icon hash
+#
+# For Icon hash, see: https://rumble.com/v119x6y-harvesting-self-service-icons.html
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
 apps=(
@@ -75,40 +73,100 @@ apps=(
 
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-# Set Dialog path and Command File
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-
-dialogApp="/usr/local/bin/dialog"
-dialog_command_file="/var/tmp/dialog.log"
-
-
-
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-# Set Overlay Icon based on Self Service icon
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-
-overlayicon=$( defaults read /Library/Preferences/com.jamfsoftware.jamf.plist self_service_app_path )
-
-
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-# Set progress_total to the number of apps in the list
-# Add 1 to progress_total for "Updating Inventory step"
+# Set progress_total to the number of apps in the list; Add one for "Updating Inventory step"
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
 progress_total=${#apps[@]}
 progress_total=$(( 1 + progress_total ))
 
 
+
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-# Set Dialog icon based on whether the Mac is a desktop or laptop
+# "Welcome Screen" Dialog Title, Message and Icon
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
+welcomeTitle="Welcome to your new Mac!"
+welcomeMessage="To begin, please enter your Mac's **Asset Tag**, then click **Continue** to start applying Church settings to your new Mac.  \n\nOnce completed, the **Quit** button will be re-enabled and you'll be prompted to restart your Mac.  \n\nIf you need assistance, please contact the GSD: +1 (801) 240-4357."
+welcomeIcon="https://avatars.githubusercontent.com/u/3598965?v=4"
+
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# "Welcome Screen" Dialog Settings and Features
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+dialogWelcomeScreenCMD="$dialogApp --ontop --title \"$welcomeTitle\" \
+--message \"$welcomeMessage\" \
+--icon \"$welcomeIcon\" \
+--iconsize 100 \
+--button1text \"Continue\" \
+--button2text \"Quit\" \
+--button2disabled \
+--infobuttontext \"v$scriptVersion\" \
+--blurscreen \
+--ontop \
+--titlefont 'size=28' \
+--messagefont 'size=18' \
+--textfield \"Asset Tag\",required,prompt=\"Please enter your Mac's Asset Tag here\" \
+--quitkey k \
+--commandfile \"$welcome_screen_command_file\""
+
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# "Setup Your Mac" Dialog Title, Message, Overlay Icon and Icon
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+title="Setting up your Mac"
+message="Please wait while the following apps are installed …"
+overlayicon=$( defaults read /Library/Preferences/com.jamfsoftware.jamf.plist self_service_app_path )
+
+# Set initial icon based on whether the Mac is a desktop or laptop
 hwType=$(/usr/sbin/system_profiler SPHardwareDataType | grep "Model Identifier" | grep "Book")  
 if [ "$hwType" != "" ]; then
   icon="SF=laptopcomputer.and.arrow.down,weight=semibold,colour1=#ef9d51,colour2=#ef7951"
 else
   icon="SF=desktopcomputer.and.arrow.down,weight=semibold,colour1=#ef9d51,colour2=#ef7951"
 fi
+
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# "Setup Your Mac" Dialog Settings and Features
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+dialogCMD="$dialogApp --ontop --title \"$title\" \
+--message \"$message\" \
+--icon \"$icon\" \
+--progress $progress_total \
+--button1text \"Quit\" \
+--button1disabled \
+--infobuttontext \"v$scriptVersion\" \
+--blurscreen \
+--ontop \
+--overlayicon \"$overlayicon\" \
+--titlefont 'size=28' \
+--messagefont 'size=14' \
+--quitkey k"
+
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Create the list of apps
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+listitems=""
+for app in "${apps[@]}"; do
+  listitems="$listitems --listitem '$(echo "$app" | cut -d '|' -f1)'"
+done
+
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Final "Setup Your Mac" Dialog to be displayed to the end-user
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+dialogCMD="$dialogCMD $listitems"
 
 
 
@@ -119,7 +177,66 @@ fi
 ####################################################################################################
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-# Execute a Dialog command
+# JAMF Display Message (for fallback in case swiftDialog fails to install)
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+function jamfDisplayMessage() {
+    echo "${1}"
+    /usr/local/jamf/bin/jamf displayMessage -message "${1}" &
+}
+
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Check for / install swiftDialog (thanks, Adam!)
+# https://github.com/acodega/dialog-scripts/blob/main/dialogCheckFunction.sh
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+function dialogCheck(){
+  # Get the URL of the latest PKG From the Dialog GitHub repo
+  dialogURL=$(curl --silent --fail "https://api.github.com/repos/bartreardon/swiftDialog/releases/latest" | awk -F '"' "/browser_download_url/ && /pkg\"/ { print \$4; exit }")
+  # Expected Team ID of the downloaded PKG
+  expectedDialogTeamID="PWA5E9TQ59"
+
+  # Check for Dialog and install if not found
+  if [ ! -e "/Library/Application Support/Dialog/Dialog.app" ]; then
+    echo "Dialog not found. Installing..."
+    # Create temporary working directory
+    workDirectory=$( /usr/bin/basename "$0" )
+    tempDirectory=$( /usr/bin/mktemp -d "/private/tmp/$workDirectory.XXXXXX" )
+    # Download the installer package
+    /usr/bin/curl --location --silent "$dialogURL" -o "$tempDirectory/Dialog.pkg"
+    # Verify the download
+    teamID=$(/usr/sbin/spctl -a -vv -t install "$tempDirectory/Dialog.pkg" 2>&1 | awk '/origin=/ {print $NF }' | tr -d '()')
+    # Install the package if Team ID validates
+    if [ "$expectedDialogTeamID" = "$teamID" ] || [ "$expectedDialogTeamID" = "" ]; then
+      /usr/sbin/installer -pkg "$tempDirectory/Dialog.pkg" -target /
+    else
+      jamfDisplayMessage "Dialog Team ID verification failed."
+      exit 1
+    fi
+    # Remove the temporary working directory when done
+    /bin/rm -Rf "$tempDirectory"  
+  else
+    echo "Dialog $(dialog --version) found; proceeding..."
+  fi
+}
+
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Execute a "Welcome Screen" Dialog command
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+function dialog_command_welcome_screen(){
+  echo "$1"
+  echo "$1"  >> $welcome_screen_command_file
+}
+
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Execute a "Setup Your Mac" Dialog command
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
 function dialog_command(){
@@ -136,15 +253,25 @@ function dialog_command(){
 function finalise(){
   dialog_command "icon: SF=checkmark.circle.fill,weight=bold,colour1=#00ff44,colour2=#075c1e"
   dialog_command "progresstext: Installation of applications complete."
-  sleep 5
+  sleep 7
   dialog_command "icon: https://ics.services.jamfcloud.com/icon/hash_90958d0e1f8f8287a86a1198d21cded84eeea44886df2b3357d909fe2e6f1296"
-  dialog_command "progresstext: Updating computer inventory …"
-  /usr/local/bin/jamf recon
+  dialog_command "progresstext: Updating computer inventory with an Asset Tag of \"${assetTag}\" …"
+
+  # If Debug Mode is enabled, pause for 7 seconds instead of updating inventory
+  if [[ $debugMode == "true" ]]; then
+    echo "DEBUG MODE IS ENABLED; otherwise would execute: /usr/local/bin/jamf recon -assetTag ${assetTag}"
+    sleep 7
+  else
+    /usr/local/bin/jamf recon -assetTag "${assetTag}"
+  fi
+
   dialog_command "icon: SF=checkmark.seal.fill,weight=bold,colour1=#00ff44,colour2=#075c1e"
-  dialog_command "progresstext: Complete"
+  dialog_command "progresstext: Complete! Please restart and enjoy your new Mac!"
   dialog_command "progress: complete"
   dialog_command "button1text: Done"
   dialog_command "button1: enable"
+  rm "$dialog_command_file"
+  rm "$welcome_screen_command_file"
   exit 0
 }
 
@@ -183,50 +310,66 @@ fi
 
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-# Construct dialog to be displayed to the end-user
+# Validate swiftDialog is installed
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
-dialogCMD="$dialogApp -p --title \"$title\" \
---message \"$message\" \
---icon \"$icon\" \
---progress $progress_total \
---button1text \"Please Wait\" \
---button1disabled \
---blurscreen \
---ontop \
---overlayicon \"$overlayicon\" \
---titlefont 'size=28' \
---messagefont 'size=14' \
---quitkey b"
+dialogCheck
 
 
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-# Create the list of apps
+# Display Welcome Screen and capture user's interaction
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
-listitems=""
-for app in "${apps[@]}"; do
-  listitems="$listitems --listitem '$(echo "$app" | cut -d '|' -f1)'"
-done
+assetTag=$( eval "$dialogWelcomeScreenCMD" | awk -F " : " '{print $NF}' )
 
-
-
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-# Final dialog to be displayed to the end-user
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-
-dialogCMD="$dialogCMD $listitems"
-echo "$dialogCMD"
+if [[ -z ${assetTag} ]]; then
+	returncode="2"
+else
+	returncode="0"
+fi
 
 
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-# Launch dialog and run it in the background; sleep for two seconds to let thing initialise
+# Evaluate User Interaction at Welcome Screen
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
-eval "$dialogCMD" &
-sleep 2
+case ${returncode} in
+
+    0)  ## Process exit code 0 scenario here
+        echo "${loggedInUser} entered an Asset Tag of ${assetTag} and clicked Continue"
+        eval "$dialogCMD" &
+        sleep 0.3
+        dialog_command "message: Asset Tag reported as \`${assetTag}\`. $message"
+        if [[ ${debugMode} == "true" ]]; then
+          dialog_command "title: DEBUG MODE | $title"
+        fi
+        ;;
+
+    2)  ## Process exit code 2 scenario here
+        echo "${loggedInUser} clicked Quit"
+        exit 0
+        ;;
+
+    3)  ## Process exit code 3 scenario here
+        echo "${loggedInUser} clicked infobutton"
+        /usr/bin/osascript -e "set Volume 3"
+        /usr/bin/afplay /System/Library/Sounds/Tink.aiff
+        ;;
+
+    4)  ## Process exit code 4 scenario here
+        echo "${loggedInUser} allowed timer to expire"
+        eval "$dialogCMD" &
+        sleep 0.3
+        ;;
+
+    *)  ## Catch all processing
+        echo "Something else happened; Exit code: ${returncode}"
+        exit 1
+        ;;
+
+esac
 
 
 
@@ -250,6 +393,15 @@ done
 
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Close Welcome Screen and pause on initial loading screen
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+dialog_command_welcome_screen "quit:"
+sleep 7
+
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 # Execute Jamf Pro Policy Events 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
@@ -257,8 +409,17 @@ done
   dialog_command "icon: https://ics.services.jamfcloud.com/icon/hash_$(echo "$app" | cut -d '|' -f4)"
   dialog_command "listitem: title: $(echo "$app" | cut -d '|' -f1), status: pending, statustext: Installing"
   dialog_command "progresstext: Installing $(echo "$app" | cut -d '|' -f1) …"
-  /usr/local/bin/jamf policy -event "$( echo "$app" | cut -d '|' -f3 )" -verbose
+
+  # If Debug Mode is enabled, pause for 7 seconds instead of executing Jamf Pro polices
+  if [[ $debugMode == "true" ]]; then
+    echo "DEBUG MODE IS ENABLED; otherwise would execute: /usr/local/bin/jamf policy -event $( echo "$app" | cut -d '|' -f3 ) -verbose"
+    sleep 7
+  else
+    /usr/local/bin/jamf policy -event "$( echo "$app" | cut -d '|' -f3 )" -verbose
+  fi
+
   appCheck &
+
 done
 
 wait)
