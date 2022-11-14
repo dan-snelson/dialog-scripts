abe524509c2f4157cbe261cf4494b1da1426094f
Dan K. Snelson <dan@snelson.us>
2022-11-14 16:15:23 -0700

    0.0.1

---
+++ swiftDialog Notifications/README.md
@@ -0,0 +1,14 @@
+# Notifications via swiftDialog
+
+> Easily display macOS Notifications with swiftDialog
+
+![Notification Screenshot](images/swiftDialog%20Notification.png)
+
+## Background
+
+Display macOS Notifications to your end-users with swiftDialog v2.
+
+[Continue reading …](https://snelson.us/)
+
+## Scripts
+- [swiftDialog Notifications.bash](swiftDialog%20Notifications.bash)
---
+++ swiftDialog Notifications/swiftDialog Notifications.bash
@@ -0,0 +1,180 @@
+#!/bin/bash
+####################################################################################################
+#
+# ABOUT
+#
+#   swiftDialog Notifications
+#
+#   See: https://snelson.us/
+#
+####################################################################################################
+#
+# HISTORY
+#
+#   Version 0.0.1, 14-Nov-2022, Dan K. Snelson (@dan-snelson)
+#       Original proof-of-concept version
+#
+####################################################################################################
+
+
+
+####################################################################################################
+#
+# Variables
+#
+####################################################################################################
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Global Variables
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+scriptVersion="0.0.1"
+export PATH=/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin/
+loggedInUser=$( echo "show State:/Users/ConsoleUser" | scutil | awk '/Name :/ { print $3 }' )
+dialogApp="/usr/local/bin/dialog"
+dialogNotificationLog=$( mktemp /var/tmp/dialogNotificationLog.XXX )
+scriptLog="${4:-"/var/tmp/org.churchofjesuschrist.log"}"
+
+if [[ -n ${5} ]]; then titleoption="--title"; title="${5}"; fi
+if [[ -n ${6} ]]; then subtitleoption="--subtitle"; subtitle="${6}"; fi
+if [[ -n ${7} ]]; then messageoption="--message"; message="${7}"; fi
+
+
+
+####################################################################################################
+#
+# Functions
+#
+####################################################################################################
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Client-side Script Logging
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+function updateScriptLog() {
+    echo -e "$( date +%Y-%m-%d\ %H:%M:%S ) - ${1}" | tee -a "${scriptLog}"
+}
+
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Quit Script (thanks, @bartreadon!)
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+function quitScript() {
+
+    updateScriptLog "Quitting …"
+
+    # Remove dialogNotificationLog
+    if [[ -e ${dialogNotificationLog} ]]; then
+        updateScriptLog "Removing ${dialogNotificationLog} …"
+        rm "${dialogNotificationLog}"
+    fi
+
+    updateScriptLog "Goodbye!"
+    exit "${1}"
+
+}
+
+
+####################################################################################################
+#
+# Pre-flight Checks
+#
+####################################################################################################
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Confirm script is running as root
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+if [[ $(id -u) -ne 0 ]]; then
+    echo "This script must be run as root; exiting."
+    exit 1
+fi
+
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Validate logged-in user
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+if [[ -z "${loggedInUser}" || "${loggedInUser}" == "loginwindow" ]]; then
+    echo "No user logged-in; exiting."
+    exit 0
+# else
+#     uid=$(id -u "${loggedInUser}")
+fi
+
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Client-side Logging
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+if [[ ! -f "${scriptLog}" ]]; then
+    touch "${scriptLog}"
+    updateScriptLog "*** Created log file via script ***"
+fi
+
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Logging preamble
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+updateScriptLog "\n\n###\n# swiftDialog Notifications (${scriptVersion})\n###\n"
+
+
+
+
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Validate Script Parameters
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+if [[ -z "${title}" ]] ; then
+
+    updateScriptLog "Parameter 5 is NOT populated; displaying instructions …"
+
+    titleoption="--title"
+    title="Title [Parameter 5] goes here"
+
+    subtitleoption="--subtitle"
+    subtitle="Subtitle [Parameter 6] goes here"
+
+    messageoption="--message"
+    message="Message [Parameter 7] goes here"
+
+else
+
+    updateScriptLog "Parameters 5, \"title,\" is populated; proceeding ..."
+
+fi
+
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Display Notification
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+updateScriptLog "Title: ${title}"
+updateScriptLog "Subtitle: ${subtitle}"
+updateScriptLog "Message: ${message}"
+
+${dialogApp} \
+    --notification \
+    ${titleoption} "${title}" \
+    ${subtitleoption} "${subtitle}" \
+    ${messageoption} "${message}" \
+    --commandfile "$dialogNotificationLog}"
+
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Exit
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+quitScript "0"
+
+exit 0
