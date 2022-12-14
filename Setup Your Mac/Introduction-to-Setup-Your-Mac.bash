351c9ffc6eeeae103ae2c055d7aadcd4a9ce540e
Dan K. Snelson <dan@snelson.us>
2022-12-14 00:40:36 -0700

    0.0.2

--- Setup Your Mac/Introduction-to-Setup-Your-Mac.bash
+++ Setup Your Mac/Introduction-to-Setup-Your-Mac.bash
@@ -12,6 +12,10 @@
 #   Version 0.0.1, 03-Dec-2022, Dan K. Snelson (@dan-snelson)
 #   - Greet the user immediately after Setup Assistant completes and before they start actually setting up their Mac
 #
+#   Version 0.0.2, 11-Dec-2022, Dan K. Snelson (@dan-snelson)
+#   - Leverages swiftDialog 2.1 to play videos as logged-in user
+#   - Updates to "Pre-flight Checks" from Setup Your Mac (1.5.1)
+#
 ####################################################################################################
 
 
@@ -22,7 +26,7 @@
 #
 ####################################################################################################
 
-scriptVersion="0.0.1"
+scriptVersion="0.0.2"
 export PATH=/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin/
 scriptLog="${4:-"/var/tmp/org.churchofjesuschrist.log"}"
 dialogBinary="/usr/local/bin/dialog"
@@ -50,12 +54,21 @@ fi
 
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Ensure computer does not go to sleep while running this script (thanks, @grahampugh!)
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+echo "Caffeinating this script (PID: $$)"
+caffeinate -dimsu -w $$ &
+
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 # Validate Setup Assistant has completed
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
-while pgrep -q -x "Setup Assistant" ; do
-    echo "Setup Assistant is running; pausing for 10 seconds"
-    sleep 10
+while pgrep -q -x "Setup Assistant"; do
+    echo "Setup Assistant is still running; pausing for 2 seconds"
+    sleep 2
 done
 
 echo "Setup Assistant is no longer running; proceeding …"
@@ -66,12 +79,12 @@ echo "Setup Assistant is no longer running; proceeding …"
 # Confirm Dock is running / user is at Desktop
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
-while ! pgrep -q -x "Dock" ; do
-    echo "Dock is NOT running; pausing for 10 seconds"
-    sleep 10
+until pgrep -q -x "Finder" && pgrep -q -x "Dock"; do
+    echo "Finder & Dock are NOT running; pausing for 1 second"
+    sleep 1
 done
 
-echo "Dock is running; proceeding …"
+echo "Finder & Dock are running; proceeding …"
 
 
 
@@ -79,13 +92,15 @@ echo "Dock is running; proceeding …"
 # Validate logged-in user
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
+loggedInUser=$( echo "show State:/Users/ConsoleUser" | scutil | awk '/Name :/ { print $3 }' )
+
 if [[ -z "${loggedInUser}" || "${loggedInUser}" == "loginwindow" ]]; then
     echo "No user logged-in; exiting."
-    quitScript "1"
+    exit 1
 else
     loggedInUserFullname=$( id -F "${loggedInUser}" )
     loggedInUserFirstname=$( echo "$loggedInUserFullname" | cut -d " " -f 1 )
-    loggedInUserID=$(id -u "${loggedInUser}")
+    # loggedInUserID=$(id -u "${loggedInUser}")
 fi
 
 
@@ -278,15 +293,6 @@ function quitScript() {
 ####################################################################################################
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-# Ensure computer does not go to sleep while running this script (thanks, @grahampugh!)
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-
-updateScriptLog "Caffeinating this script (PID: $$)"
-caffeinate -dimsu -w $$ &
-
-
-
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 # Validate swiftDialog is installed
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
@@ -298,8 +304,8 @@ dialogCheck
 # Display Introduction dialog
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
-# eval "${dialogBinary} --args ${dialogIntroduction}"
-su - "${loggedInUser}" -c "open -a ${dialogBinary} --args ${dialogIntroduction} & sleep 0.35"
+eval "${dialogBinary} --args ${dialogIntroduction}"
+# su - "${loggedInUser}" -c "open -a ${dialogBinary} --args ${dialogIntroduction} & sleep 0.35"
 
 
 
