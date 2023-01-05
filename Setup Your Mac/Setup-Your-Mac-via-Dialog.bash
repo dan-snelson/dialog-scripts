602800fec1dffb39752f9d99154ecd781babad46
mattdjerome <mattdjerome@gmail.com>
2023-01-05 12:01:43 -0800

    added processor type check

--- Setup Your Mac/Setup-Your-Mac-via-Dialog.bash
+++ Setup Your Mac/Setup-Your-Mac-via-Dialog.bash
@@ -34,6 +34,17 @@ if [[ $(id -u) -ne 0 ]]; then
     exit 1
 fi
 
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Determine Processor Type
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+cpu=$(sysctl -a | grep brand | awk '{print $2}')
+echo "CPU vendor is $cpu"
+if [[ $cpu = "Apple"  ]]; then
+    type="arm"
+else
+    type="intel"
+fi
 
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
@@ -419,6 +430,17 @@ policy_array=('
             "trigger_list": [
                 {
                     "trigger": "code42_${type}",
+                    "path": ""
+                }
+            ]
+        },        
+        {
+            "listitem": "Install Slack",
+            "icon": "a1ecbe1a4418113177cc061def4996d20a01a1e9b9adf9517899fcca31f3c026",
+            "progresstext": "Install Slack Messaging System",
+            "trigger_list": [
+                {
+                    "trigger": "install_slack",
                     "path": ""
                 }
             ]
