c15cacaa1487b8d85f6873d9aadf24032e7e0aa2
Andrew Barnett <138056529+AndrewMBarnett@users.noreply.github.com>
2023-08-15 10:20:11 -0500

    Update swiftDialog Pre-install.bash
    Added the file removal for the /var/tmp/dialog.log and added install function

--- swiftDialog Notifications/swiftDialog Pre-install.bash
+++ swiftDialog Notifications/swiftDialog Pre-install.bash
@@ -119,7 +119,72 @@ case ${installationAction} in
         updateScriptLog "Removing swiftDialog …"
         rm -fv /usr/local/bin/dialog
         rm -Rfv /Library/Application\ Support/Dialog/
+        rm -fv /var/tmp/dialog.log
         updateScriptLog "swiftDialog has been removed"
+        ;;
+
+     "install" )
+        updateScriptLog "Installing new swiftDialog version …"
+
+function dialogCheck() {
+
+    # Output Line Number in `true` Debug Mode
+    #if [[ "${debugMode}" == "true" ]]; then updateScriptLog "WARM-UP: # # # Patch Helper true DEBUG MODE: Line No. ${LINENO} # # #" ; fi
+
+    # Get the URL of the latest PKG From the Dialog GitHub repo
+    dialogURL=$(curl --silent --fail "https://api.github.com/repos/bartreardon/swiftDialog/releases/latest" | awk -F '"' "/browser_download_url/ && /pkg\"/ { print \$4; exit }")
+
+    # Expected Team ID of the downloaded PKG
+    expectedDialogTeamID="PWA5E9TQ59"
+
+    # Check for Dialog and install if not found
+    if [ ! -e "/Library/Application Support/Dialog/Dialog.app" ]; then
+
+        updateScriptLog "WARM-UP: Dialog not found. Installing..."
+
+        # Create temporary working directory
+        workDirectory=$( /usr/bin/basename "$0" )
+        tempDirectory=$( /usr/bin/mktemp -d "/private/tmp/$workDirectory.XXXXXX" )
+
+        # Download the installer package
+        /usr/bin/curl --location --silent "$dialogURL" -o "$tempDirectory/Dialog.pkg"
+
+        # Verify the download
+        teamID=$(/usr/sbin/spctl -a -vv -t install "$tempDirectory/Dialog.pkg" 2>&1 | awk '/origin=/ {print $NF }' | tr -d '()')
+
+        # Install the package if Team ID validates
+        if [[ "$expectedDialogTeamID" == "$teamID" ]]; then
+
+            /usr/sbin/installer -pkg "$tempDirectory/Dialog.pkg" -target /
+            sleep 2
+            dialogVersion=$( /usr/local/bin/dialog --version )
+            updateScriptLog "WARM-UP: swiftDialog version ${dialogVersion} installed; proceeding..."
+
+        else
+
+            # Display a so-called "simple" dialog if Team ID fails to validate
+            osascript -e 'display dialog "Please advise your Support Representative of the following error:\r\r• Dialog Team ID verification failed\r\r" with title "Swift Dialog: Error" buttons {"Close"} with icon caution'
+            completionActionOption="Quit"
+            exitCode="1"
+            quitScript
+
+        fi
+
+        # Remove the temporary working directory when done
+        /bin/rm -Rf "$tempDirectory"
+
+    else
+
+        updateScriptLog "WARM-UP: swiftDialog version $(/usr/local/bin/dialog --version) found; proceeding..."
+
+    fi
+    }
+    if [[ ! -e "/Library/Application Support/Dialog/Dialog.app" ]]; then
+    dialogCheck
+else
+    updateScriptLog "WARM-UP: swiftDialog version $(/usr/local/bin/dialog --version) found; proceeding..."
+fi
+        updateScriptLog "swiftDialog has been installed"
         ;;
 
     "none" | * )
